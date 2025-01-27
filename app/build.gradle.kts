ce1e62d2baa8dde5db5137dbf2def3577ba0eb57
OlegEfremov54 <ole_f@mail.ru>
2025-01-27 20:53:27 +0300

    Комитк 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.dz16bottomnavigationview"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.dz16bottomnavigationview"

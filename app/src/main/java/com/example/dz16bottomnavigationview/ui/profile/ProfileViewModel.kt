1ef70d087eabb4c70bda66101dfd0ee069ed94c0
OlegEfremov54 <ole_f@mail.ru>
2025-01-27 22:56:05 +0300

    Комитк 1

--- app/src/main/java/com/example/dz16bottomnavigationview/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/dz16bottomnavigationview/ui/profile/ProfileViewModel.kt
@@ -4,10 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ProfileViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
 }
